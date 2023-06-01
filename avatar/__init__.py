2e6cf8336450a95727454fa4c0a717db0fb94934
Lucas Abel <22837557+uael@users.noreply.github.com>
2023-06-01 17:38:27 +0000

    bumb version to 0.0.2

--- avatar/__init__.py
+++ avatar/__init__.py
@@ -17,7 +17,7 @@ Avatar is a scalable multi-platform Bluetooth testing tool capable of running
 any Bluetooth test cases virtually and physically.
 """
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 import enum
 import functools
