e142121b1ab7e6038078cdd2975a21be0f9915cf
Qiao Yang <qiaoccolato@google.com>
2023-06-06 10:42:20 -0700

    host_test: disable connect/disconnect for android DUT, add test_accept

--- cases/host_test.py
+++ cases/host_test.py
@@ -18,7 +18,7 @@ import grpc
 import logging
 
 from avatar import BumblePandoraDevice, PandoraDevice, PandoraDevices
-from mobly import base_test, test_runner
+from mobly import base_test, signals, test_runner
 from mobly.asserts import assert_equal  # type: ignore
 from mobly.asserts import assert_false  # type: ignore
 from mobly.asserts import assert_is_none  # type: ignore
@@ -87,6 +87,8 @@ class HostTest(base_test.BaseTestClass):  # type: ignore[misc]
 
     @avatar.asynchronous
     async def test_connect(self) -> None:
+        if self.dut.name == 'android':
+            raise signals.TestSkip('TODO: Android connection is too flaky (b/285634621)')
         ref_dut_res, dut_ref_res = await asyncio.gather(
             self.ref.aio.host.WaitConnection(address=self.dut.address),
             self.dut.aio.host.Connect(address=self.ref.address),
@@ -98,7 +100,21 @@ class HostTest(base_test.BaseTestClass):  # type: ignore[misc]
         assert_true(await self.is_connected(self.ref, ref_dut), "")
 
     @avatar.asynchronous
+    async def test_accept(self) -> None:
+        dut_ref_res, ref_dut_res = await asyncio.gather(
+            self.dut.aio.host.WaitConnection(address=self.ref.address),
+            self.ref.aio.host.Connect(address=self.dut.address),
+        )
+        assert_is_not_none(ref_dut_res.connection)
+        assert_is_not_none(dut_ref_res.connection)
+        ref_dut, dut_ref = ref_dut_res.connection, dut_ref_res.connection
+        assert ref_dut and dut_ref
+        assert_true(await self.is_connected(self.ref, ref_dut), "")
+
+    @avatar.asynchronous
     async def test_disconnect(self) -> None:
+        if self.dut.name == 'android':
+            raise signals.TestSkip('TODO: Android disconnection is too flaky (b/286081956)')
         dut_ref_res, ref_dut_res = await asyncio.gather(
             self.dut.aio.host.WaitConnection(address=self.ref.address),
             self.ref.aio.host.Connect(address=self.dut.address),
