43870c8149733a8840450450a606bc4becd5d85f
Charlie Boutier <charliebout@google.com>
2024-03-14 20:57:46 +0000

    bump to version 0.0.8

--- avatar/__init__.py
+++ avatar/__init__.py
@@ -17,7 +17,7 @@ Avatar is a scalable multi-platform Bluetooth testing tool capable of running
 any Bluetooth test cases virtually and physically.
 """
 
-__version__ = "0.0.5"
+__version__ = "0.0.8"
 
 import argparse
 import enum
